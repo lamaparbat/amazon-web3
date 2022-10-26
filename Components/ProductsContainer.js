9e075f34a16cd28212c6390c9f9d7133cc0944ab
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2022-10-26 10:20:43 +0545

    Add responsive breakpoints

--- Components/ProductsContainer.js
+++ Components/ProductsContainer.js
@@ -5,9 +5,9 @@ import { nftsCollections } from '../constants';
 const ProductsContainer = () => {
  return (
   <div className='py-3 w-100 h-full bg-slate-100 flex justify-center'>
-   <div className='w-[1000px]'>
+   <div className='px-2 lg:w-[1000px] md:w-[800px] sm:w-[500px] '>
     <h4 className='mb-4'>NFT Marketplaces</h4>
-    <div className='w-[950px] flex flex-wrap justify-between'>
+    <div className='grid lg:w-[950px] lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2'>
      {
       nftsCollections.map((data, i) => {
        const { img, name } = data;
